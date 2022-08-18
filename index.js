97a12f603ca8f6464c220e99686c70a92a03b02b
SelinD <95566006+Melpemone@users.noreply.github.com>
2022-08-18 21:16:57 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const piu = document.getElementById("piu");
+
+var i = 0;
+var text = "WELCOME!";
+var speed = 90;
+
+function myFunc() {
+  if (i < text.length) {
+    document.querySelector("p").innerHTML += text.charAt(i);
+    i++;
+
+    setTimeout(myFunc, speed);
+
+    document.querySelector("p").onmouseout = function () {
+      document.querySelector("p").innerHTML = text.replace(
+        "WELCOME!",
+        "&#169; SHF, 2022 "
+      );
+    };
+  }
+}
